a7d6fcd748f897b30dc0f3e8f673a138f5a8e871
Romain Tartière <romain@blogreen.org>
2019-03-01 11:29:10 -1000

    Improve function idempotency
    PuppetDB results are unordered by default, so the value returned by
    certificate_checker::watched_paths() may change from time to time.  To avoid
    this, remove duplicate items and sort the array alphabetically.
    
    While here, use dig where applicable.

--- functions/watched_paths.pp
+++ functions/watched_paths.pp
@@ -1,4 +1,4 @@
 function certificate_checker::watched_paths() >> Array[Stdlib::Absolutepath] {
   $pql = "resources [parameters] { type = 'Certificate_checker::Watch' and certname = '${trusted['certname']}' }"
-  puppetdb_query($pql).map |$value| { $value['parameters']['paths'] }.flatten()
+  puppetdb_query($pql).map |$value| { $value.dig('parameters', 'paths') }.flatten().unique().sort()
 }
