aeba511602129b5bea9fafac84745b349b4f5fe8
Romain Tartière <romain@blogreen.org>
2020-01-02 17:01:10 -1000

    Update from opus-codium modulesync_config

--- manifests/watch.pp
+++ manifests/watch.pp
@@ -1,3 +1,6 @@
+# @summary Add certificates to monitor
+#
+# @param paths Paths to certificates to monitor
 define certificate_checker::watch (
   Variant[Array[Stdlib::Absolutepath], Stdlib::Absolutepath] $paths = $title,
 ) {
